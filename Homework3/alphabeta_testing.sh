f2ff9bfdfba60a37e79b90231d2f910a79bb1116
Bryan John Baker <bryan.j.baker@seagate.com>
2021-10-12 23:04:24 -0500

    Created some testing files.

---
+++ Homework3/alphabeta_testing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+white=0
+black=0
+value=""
+for i in {1..100}
+do
+	# echo -n $i
+	value=$(python3 alphabeta_vs_random.py | grep wins | awk '{print $1;}')
+	if [[ "$value" == "White" ]]
+	then
+		let white+=1
+	else
+		let black+=1
+	fi
+done
+echo "black=$black"
+echo "white=$white"
