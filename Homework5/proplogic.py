5b35321b86fab4d20e61b88465aa83e957669407
Bryan John Baker <bryan.j.baker@seagate.com>
2021-11-10 15:56:47 -0600

    Added comments to proplogic.py file.

--- Homework5/proplogic.py
+++ Homework5/proplogic.py
@@ -12,6 +12,17 @@ import sat_interface
 
 def example_problem():
     print("Liars and Truth-tellers Example Problem:")
+    """
+    List of variables:
+    A - Amy is telling the truth (A) or not (~A)
+    B - Bob is telling the truth (B) or not (~B)
+    C - Cal is telling the truth (C) or not (~C)
+    
+    List of statements:
+    A <=> ~B
+    B <=> ~C
+    C <=> (~A and ~B)
+    """
     example_prob = sat_interface.KB(["~A ~B", "B A", "~B ~C", "C B", "~C ~A", "~C ~B", "A B C"])
     if example_prob.is_satisfiable():
         print("The knowledge base is satisfiable.")
@@ -41,6 +52,17 @@ def example_problem():
 
 def tt2():
     print("Liars and Truth-tellers II:")
+    """
+    List of variables:
+    A - Amy is telling the truth (A) or not (~A)
+    B - Bob is telling the truth (B) or not (~B)
+    C - Cal is telling the truth (C) or not (~C)
+
+    List of statements:
+    A <=> (A and C)
+    B <=> ~C
+    C <=> (B or ~A)
+    """
     example_prob = sat_interface.KB(["~A C", "~A A ~C", "~B ~C", "C B", "~C B ~A", "~B C", "A C"])
     if example_prob.is_satisfiable():
         print("The knowledge base is satisfiable.")
@@ -70,6 +92,17 @@ def tt2():
 
 def tt3():
     print("Liars and Truth-tellers III:")
+    """
+    List of variables:
+    A - Amy is telling the truth (A) or not (~A)
+    B - Bob is telling the truth (B) or not (~B)
+    C - Cal is telling the truth (C) or not (~C)
+
+    List of statements:
+    A <=> ~C
+    B <=> (A and C)
+    C <=> B
+    """
     example_prob = sat_interface.KB(["~A ~C", "C A", "~B A", "~B C", "~A ~C B", "~C B", "~B C"])
     if example_prob.is_satisfiable():
         print("The knowledge base is satisfiable.")
@@ -99,6 +132,26 @@ def tt3():
 
 def salt():
     print("Robbery and a Salt:")
+    """
+    List of variables:
+    A - Caterpillar ate the salt (A) or not (~A)
+    B - Bill the Lizard ate the salt (B) or not (~B)
+    C - Cheshire Cat ate the salt (C) or not (~C)
+    AT - Caterpillar is telling the truth (AT) or not (~AT)
+    BT - Bill the Lizard is telling the truth (BT) or not (~BT)
+    CT - Cheshire Cat is telling the truth (CT) or not (~CT)
+
+    List of statements:
+    AT <=> B
+    ~AT <=> A or C
+    BT <=> AT
+    ~BT <=> ~AT
+    CT <=> ~C
+    ~CT <=> C
+    A or B or C
+    AT or BT or CT
+    ~AT or ~BT or ~CT
+    """
     example_prob = sat_interface.KB(["~AT B", "~B AT", "AT A C", "~A ~AT", "~C ~AT", "~BT AT", "~AT BT"
                                             , "~CT ~C", "C CT", "A B C", "AT BT CT", "~AT ~BT ~CT"])
     if example_prob.is_satisfiable():
@@ -147,6 +200,43 @@ def salt():
 
 def golf():
     print("An honest name:")
+    """
+    List of variables:
+    T - Tom is telling the truth (T) or not (~T)
+    D - Dick is telling the truth (D) or not (~D)
+    H - Harry is telling the truth (H) or not (~H)
+    F - The first man is telling the truth (F) or not (~F)
+    M - The middle man is telling the truth (M) or not (~M)
+    L - The last man is telling the truth (L) or not (~L)
+    TF - Tom is the first man true (TF) or false (~TF)
+    TM - Tom is the middle man true (TM) or false (~TM)
+    TL - Tom is the last man true (TL) or false (~TL)
+    DF - Dick is the first man true (DF) or false (~DF)
+    DM - Dick is the middle man true (DM) or false (~DM)
+    DL - Dick is the last man true (DL) or false (~DL)
+    HF - Harry is the first man true (HF) or false (~HF)
+    HM - Harry is the middle man true (HM) or false (~HM)
+    HL - Harry is the last man true (HL) or false (~HL)
+
+    List of statements:
+    T
+    ~H
+    T <=> ((F <=> T) and (M <=> H) and (L <=> D))
+    ~T <=> ((F <=> (D or H)) and (M <=> (T or D)))
+    H <=> ((L <=> H) and (M <=> T))
+    F <=> (M <=> H)
+    L <=> (M <=> T)
+    F or M or L
+    TF <=> (M <=> H)
+    TM <=> ~(M <=> D)
+    TL <=> (M <=> T)
+    DF <=> ~(M <=> H)
+    DM <=> ~(M <=> D)
+    DL <=> ~(M <=> T)
+    HF <=> ~(M <=> H)
+    HM <=> (M <=> D)
+    HL <=> (M <=> T)
+    """
     example_prob = sat_interface.KB(["T", "D ~D", "~H"
                                     , "F ~H ~M", "F H M", "~H M ~T", "H ~M ~T"
                                     , "~D F ~H ~L ~M", "~D F H ~L M", "~D L ~T", "D F ~H L ~M", "D F H L M", "D ~L ~T", "~H M ~T", "H ~M ~T"
