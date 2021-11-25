c979237639d7a0ddc8f37ba2ec2817fabdb2cd0c
Bryan John Baker <bake1358@umn.edu>
2021-11-24 22:21:26 -0600

    Created perm predicate.

--- Homework7/fol.pl
+++ Homework7/fol.pl
@@ -1,8 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Bryan John Baker
 % CSCI 5511
 % Homework 7 - First-Order Logic: Prolog
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Kinship Domain
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 parent(charles, william).
 parent(charles, harry).
 parent(elizabeth, charles).
@@ -30,3 +34,42 @@ cousin(X, Y) :- parent(A, X), parent(B, Y), sibling(A, B), not(A=B).
 % Define the ancestor predicate
 ancestor(X, Z) :- parent(X, Z).
 ancestor(X, Z) :- parent(Y, Z), ancestor(X, Y).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Basic Length Definition
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+len([], 0).
+len([_ | T], N) :- len(T, M), N is M+1.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Sorted predicate
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sorted([]).
+sorted([_]).
+sorted([H, A | T]) :- H =< A, sorted([A|T]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Perm predicate 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+is_in(X, [H|_]) :- X = H, !.
+is_in(X, [_|T]) :- is_in(X, T).
+
+remove_item(A, [A|B], B).
+remove_item(A, [B|C], [B|D]) :- remove_item(A, C, D).
+
+perm([],[]).
+perm([H1|T1], [H2|T2]) :- 
+    len([H1|T1], X), 
+    len([H2|T2], X), 
+    is_in(H1, [H2|T2]), 
+    is_in(H2, [H1|T1]), 
+    remove_item(H1, [H2|T2], Y), 
+    perm(Y, T1).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Mysort predicate 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Mysort time complexity
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
